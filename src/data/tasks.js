e8232b76c03a44c51d83a7b191ff42161fe5bb1e
Nhial Cham <nhialcham14@gmail.com>
2026-06-10 17:52:57 +0200

    Dataset - task list

---
+++ src/data/tasks.js
@@ -0,0 +1,19 @@
+const tasks = [
+    {
+        id: 1,
+        title: "Builder navbar",
+        status: "Completed",
+    },
+    {
+        id: 2,
+        title: "create Dashboard",
+        status: "In Progress",
+    },
+    {
+        id: 3,
+        title: "Implement Router",
+        status: "Pending",
+    }
+];
+
+export default tasks;
